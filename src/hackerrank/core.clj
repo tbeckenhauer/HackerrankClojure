2f7911f13f033960443054a8cfd6040e9a0b498c
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2022-09-23 19:17:41 -0400

    fp-filter-array: clojure

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -6,8 +6,16 @@
    (reduce
     (fn [frst scnd] (conj frst (repeat num scnd))) [] lst)))
 
+(defn fp-filter-array [delim lst]
+  (for 
+    [i lst :when (> delim i)] 
+    i))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (fp-list-replication 3 [1 3 5])))
+  )
 
+(defn old-tests
+  (println (fp-filter-array 3 [1 2 3 4 5]))
+  (println (fp-list-replication 3 [1 3 5])))
