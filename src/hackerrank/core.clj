75be83cca1ee9ccd116760f69cdb4b0397403b69
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2022-09-23 18:40:17 -0400

    fp-list-replication: clojure

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -1,7 +1,13 @@
 (ns hackerrank.core
   (:gen-class))
 
+(defn fp-list-replication [num lst]
+  (flatten
+   (reduce
+    (fn [frst scnd] (conj frst (repeat num scnd))) [] lst)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (fp-list-replication 3 [1 3 5])))
+
