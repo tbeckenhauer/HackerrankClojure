3aaf58bf26c9733ea5b72fdd6981007a40fa64d7
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2022-09-23 19:53:54 -0400

    fp-filter-positions-in-a-list: clojure

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -11,11 +11,14 @@
     [i lst :when (> delim i)] 
     i))
 
+(defn fp-filter-positions-in-a-list [lst]
+   (take-nth 2 (drop 1 lst)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  )
+  (println (fp-filter-positions-in-a-list [0 1 2 3 4 5 6 7 8 9])))
 
-(defn old-tests
-  (println (fp-filter-array 3 [1 2 3 4 5]))
-  (println (fp-list-replication 3 [1 3 5])))
+;; (defn old-tests
+  ;; (println (fp-filter-array 3 [1 2 3 4 5]))
+  ;; (println (fp-list-replication 3 [1 3 5])))
