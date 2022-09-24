5ab2c3e355477502319dcab215ad560c2e609087
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2022-09-23 20:54:57 -0400

    fp-reverse-a-list: clojure

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -14,11 +14,21 @@
 (defn fp-filter-positions-in-a-list [lst]
    (take-nth 2 (drop 1 lst)))
 
+(defn fp-reverse-a-list [lst]
+   (flatten 
+   (remove nil?
+    [(last lst)
+    (if (empty? lst)
+      []
+      (fp-reverse-a-list (drop-last lst)))])
+      ))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (fp-filter-positions-in-a-list [0 1 2 3 4 5 6 7 8 9])))
+  (println (fp-reverse-a-list [0 1 2 3 4 5 6 7 8 9])))
 
 ;; (defn old-tests
+  ;; (println (fp-filter-positions-in-a-list [0 1 2 3 4 5 6 7 8 9]))
   ;; (println (fp-filter-array 3 [1 2 3 4 5]))
   ;; (println (fp-list-replication 3 [1 3 5])))
